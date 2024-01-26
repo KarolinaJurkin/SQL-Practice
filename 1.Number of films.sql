81329ee066a506f47198fb4a8ecc148dbb3869a5
Karolina Jurkin <corpsecally@gmail.com>
2024-01-26 15:34:43 +0100

    Count number of films actors starred in (JOIN+COUNT)
    Practice question from EPAM "Data & Analytics. Introduction to SQL"

---
+++ 1.Number of films.sql
@@ -0,0 +1,14 @@
+-- Count number of films actors starred in
+
+SELECT 
+	a.first_name || ' ' || a.last_name AS actor,
+	COUNT(fa.film_id) AS number_of_films
+FROM
+	film_actor fa
+	JOIN actor a
+	ON	fa.actor_id=a.actor_id
+GROUP BY
+	actor
+ORDER BY
+	COUNT(fa.film_id) DESC;
+
